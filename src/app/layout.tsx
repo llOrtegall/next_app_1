685ed47217f221fc0ee9e5978a71646a04089e66
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-12 21:29:42 -0500

    add icons and new header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import Header from '@/components/Header';
+import localFont from 'next/font/local';
+import type { Metadata } from 'next';
+import './globals.css';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Markeplate Next.js",
-  description: "learn next.js, and create a marketplace",
+  title: 'Markeplate Next.js',
+  description: 'learn next.js, and create a marketplace',
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
+        <Header />
         {children}
       </body>
     </html>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,19 @@
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+
 export default function Header() {
   return(
-    <header>
-      
+    <header className='border-b p-4 flex items-center justify-between'>
+      <Link href='/' className='text-blue-600 font-semibold text-2xl'>Markeplace</Link>
+      <nav className='flex gap-4 *:border  *:rounded *:px-2  *:py-1'>
+        <button className='border-blue-600 text-blue-600 inline-flex items-center gap-1'>
+          <FontAwesomeIcon icon={faPlus} className='h-4' />
+          <span>Post a ad</span>
+        </button>
+        <button className=''>Login</button>
+        <button className=''>Register</button>
+      </nav>
     </header>
   )
 }
